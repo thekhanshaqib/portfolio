d1286836a7dbfbd038315ec7d4b66920dcb57719
Shaqib Iqbal <shaqibiqbal.1212@gmail.com>
2026-03-02 19:01:36 +0000

    AIzaSyA8gFxECUnBZjeLzv0DRWVPDUuBpGin4bo this is my key

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,7 +4,7 @@ import {googleAI} from '@genkit-ai/google-genai';
 export const ai = genkit({
   plugins: [
     googleAI({
-      apiKey: process.env.GOOGLE_GENAI_API_KEY || process.env.GEMINI_API_KEY,
+      apiKey: process.env.GOOGLE_GENAI_API_KEY || process.env.GEMINI_API_KEY || "AIzaSyA8gFxECUnBZjeLzv0DRWVPDUuBpGin4bo",
     }),
   ],
   model: 'googleai/gemini-2.5-flash',
